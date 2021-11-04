a1bb54f99ae8ea03c2403d18c7e27c3a9427f0af
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2021-11-03 23:16:01 -0300

    Authentication title updated

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -1,7 +1,7 @@
+import 'package:dontpanic/screens/user_info_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterfire_samples/screens/user_info_screen.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
